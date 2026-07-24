143346b742403f103b2521096ae71c104d05b747
Shun Okada <falcon0296@gmail.com>
2026-07-25 02:20:21 +0900

    feat: add getAdjacentChapterRef for chapter navigation across book/front-matter boundaries

--- apps/pwa/src/entities/scripture/index.ts
+++ apps/pwa/src/entities/scripture/index.ts
@@ -1,2 +1,3 @@
 export type { ScriptureRef } from './lib/scriptureUtils'
 export { buildScriptureUrl, getScriptureLabel, getCollection, getBook, getAllCollections } from './lib/scriptureUtils'
+export { getAdjacentChapterRef, type ChapterRef } from './lib/scriptureNavigation'
